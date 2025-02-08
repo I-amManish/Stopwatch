045288085cb60c32b2c5f1069eb70515addf0f42
Manish <manishkumaryt03@gmail.com>
2025-02-08 11:20:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Stopwatch from './Stopwatch.jsx'
+
+function App() {
+
+
+  return (
+    <Stopwatch/>
+  )
+}
+
+export default App
---
+++ src/Stopwatch.jsx
@@ -0,0 +1,65 @@
+import {useEffect, useState, useRef } from "react";
+
+function Stopwatch(){
+    const [isRunning, setIsRunning] = useState(false);
+    const [elapsedTime, setelapsedTime] = useState(0);
+    const intervalIdRef = useRef(null);
+    const startTimeRef = useRef(0);
+
+    useEffect(()=>{
+        if(isRunning){
+            intervalIdRef.current = setInterval(() => {
+                setelapsedTime(Date.now()- startTimeRef.current);
+            }, 10);
+        }
+
+        // cleanup function
+        return() =>{
+            clearInterval(intervalIdRef.current);
+        }
+    },[isRunning]);
+
+    function start(){
+        setIsRunning(true);
+        startTimeRef.current = Date.now() - elapsedTime;
+        
+    }
+    function stop(){
+        setIsRunning(false);
+    }
+
+    function reset(){
+        setelapsedTime(0);
+        setIsRunning(false)
+    }
+
+    function formateTime(){
+        let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+        let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+        let seconds = Math.floor(elapsedTime / (1000) % 60);
+        let millisecondSeconds = Math.floor(elapsedTime % (1000) / 10);
+
+        hours = String(hours).padStart(2, "0")
+        minutes = String(minutes).padStart(2, "0")
+        seconds = String(seconds).padStart(2, "0")
+        millisecondSeconds = String(millisecondSeconds).padStart(2, "0")
+
+        return `${hours}:${minutes}:${seconds}:${millisecondSeconds}` ;
+    }
+
+    return(
+        <>
+            <div className="stopwatch">
+                <div className="display">{formateTime()}</div>
+                <div className="controls">
+                    <button onClick={start} className="start-button">Start</button>
+                    <button onClick={stop} className="stop-button">Stop</button>
+                    <button onClick={reset} className="reset-button">Reset</button>
+                </div>
+            </div>
+        </>
+    );
+
+}
+
+export default Stopwatch
